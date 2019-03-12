5d69f11731e4ef184a6c74dfec5f8d22a7624d4a
matthew.serre <matthew.serre@gmail.com>
2019-03-12 00:54:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT Characters.name, COUNT(character_books.id) from Characters INNER JOIN character_books on Characters.id = character_books.character_id GROUP BY Characters.name"
+  "SELECT Characters.name, COUNT(character_books.id) DESC from Characters INNER JOIN character_books on Characters.id = character_books.character_id GROUP BY Characters.name"
 end
